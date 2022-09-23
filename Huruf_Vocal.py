42849c2d27e4f748af8ccae0762c592bd486d147
filbertcha <113040620+filbertcha@users.noreply.github.com>
2022-09-23 13:30:09 +0700

    Update Huruf_Vocal.py

--- Huruf_Vocal.py
+++ Huruf_Vocal.py
@@ -1,6 +1,6 @@
-tu = "aiueo"
-hi = input("ketik bang: ").lower().strip()
-for letter in hi:
-    if letter in tu:
-        hi = hi.replace(letter,"")
-print(hi)
+vocal = "aiueo"
+a = input("ketik bang: ").lower().strip()
+for letter in a:
+    if letter in vocal :
+        a = a.replace(letter,"")
+print(a)
